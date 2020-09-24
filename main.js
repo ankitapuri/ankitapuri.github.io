9f79553d16ee892c43650e51873d31deac62a953
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2020-09-24 09:42:48 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// For toggle button;
+
+function toggleClass()
+{
+
+    const body = document.querySelector('body');
+    body.classList.toggle('light');
+    body.style.transition = `0.3s linear`;
+}
+
+
+
+
+
+
+
+
