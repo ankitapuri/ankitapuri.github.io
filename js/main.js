a7861011619c8defea0a52edc47632a28575fcf4
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2020-10-23 21:45:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['I am into Designing.', 'I am into Web Development.', 'I am into Open Source.'], 'text',['white','white','white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
