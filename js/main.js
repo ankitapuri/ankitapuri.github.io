8835306b8f5f88ee2f921a6609ce035055ac22ee
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2020-10-24 00:49:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function consoleText(words, id, colors) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
-  }, 120)
+  }, 90)
   window.setInterval(function() {
     if (visible === true) {
       con.className = 'console-underscore hidden'
