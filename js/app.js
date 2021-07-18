fbfebcb825bd192d6710e018a1671062b75428f1
ankita <puriankita30@gmail.com>
2021-07-18 17:40:53 +0530

    added

--- js/app.js
+++ js/app.js
@@ -1,133 +1,177 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+/* ---- particles.js config ---- */
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
+particlesJS('particles-js', {
+	particles: {
+		number: {
+			value: 140,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		},
+		color: {
+			value: [ '#1e3d59', '#f5f0e1', '#ff6e40', '#ffc13b' ]
+		},
+		shape: {
+			type: 'circle',
+			stroke: {
+				width: 0,
+				color: '#fff'
+			},
+			polygon: {
+				nb_sides: 5
+			},
+			image: {
+				src: 'https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png',
+				width: 70,
+				height: 70
+			}
+		},
+		
+		opacity: {
+			value: 1,
+			random: false,
+			anim: {
+				enable: false,
+				speed: 1,
+				opacity_min: 0.1,
+				sync: false
+			}
+		},
+		size: {
+			value: 8,
+			random: true,
+			anim: {
+				enable: false,
+				speed: 10,
+				size_min: 10,
+				sync: false
+			}
+		},
+		line_linked: {
+			enable: true,
+			distance: 150,
+			color: '#808080',
+			opacity: 0.4,
+			width: 1
+		},
+		move: {
+			enable: true,
+			speed: 5,
+			direction: 'none',
+			random: false,
+			straight: false,
+			out_mode: 'out',
+			bounce: false,
+			attract: {
+				enable: false,
+				rotateX: 600,
+				rotateY: 1200
+			}
+		}
+	},
+	interactivity: {
+		detect_on: 'window',
+		events: {
+			onhover: {
+				enable: true,
+				mode: 'repulse'
+			},
+			onclick: {
+				enable: true,
+				mode: 'push'
+			}
+		},
+		modes: {
+			'repulse' : {
+				distance: 70,
+				duration: 0.4
+			},
+			'push' : {
+				particles_nb: 4
+			}
+		}
+	},
+	retina_detect: true
 });
-*/
+const allElements = document.querySelectorAll('.animated-text');
 
-/* Otherwise just put the config content (json): */
+// It checks if there is at least one element
+if (allElements.length > 0) {
+	//It runs the script for each found element
+	allElements.forEach((element) => {
+		const txtElement = element,
+			wordsList = txtElement.getAttribute('data-words'),
+			words = wordsList.split(', '); // It makes an array of words from data attribute
 
-particlesJS('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
+		let wordsCount = 0;
 
-);
+		entry();
+
+		// Initial function
+		function entry() {
+			if (wordsCount < words.length) {
+				// It runs the code for each word
+				let word = words[wordsCount],
+					txtArr = word.split(''), // It makes an array of letters in the word
+					count = 0;
+
+				txtElement.textContent = ''; // It removes the previous text from the element
+
+				// For each letter in the array
+				txtArr.forEach((letter) => {
+					// It replaces the empty space for the "non-break-space" HTML...
+					// ... This is needed to separate the words properly
+					let _letter = letter === ' ' ? '&nbsp;' : letter;
+
+					// It wraps every letter with a "span" and puts all they back to the element
+					txtElement.innerHTML += `<span>${_letter}</span>`;
+				});
+
+				let spans = txtElement.childNodes;
+
+				// It sets the interval between each letter showing
+				const letterInterval = setInterval(activeLetter, 70);
+
+				function activeLetter() {
+					spans[count].classList.add('active');
+					count++;
+
+					if (count === spans.length) {
+						clearInterval(letterInterval);
+
+						// It waits 4 seconds to start erasing the word
+						setTimeout(() => {
+							eraseText();
+						}, 600);
+					}
+				}
+
+				function eraseText() {
+					// It sets the interval between each letter hiding
+					let removeInterval = setInterval(removeLetter, 40);
+					count--;
+
+					function removeLetter() {
+						spans[count].classList.remove('active');
+						count--;
+
+						if (count === -1) {
+							clearInterval(removeInterval);
+							wordsCount++;
+
+							// After removing the last letter, call the initial function again
+							entry();
+						}
+					}
+				}
+			} else {
+				// If the code reaches the last word
+				// It resets the words counter...
+				wordsCount = 0;
+				// ...and calls the initial function again.
+				entry();
+			}
+		}
+	});
+}
